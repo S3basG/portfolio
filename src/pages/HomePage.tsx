f6dec0cf5e3639b82fc85822994223d182e35e40
SebasG <sgarcia190204@gmail.com>
2026-09-04 17:20:03 -0400

    Replace unused starter page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,8 +1,1 @@
-export default function HomePage() {
-  return (
-    <div style={{ padding: 24 }}>
-      <h1>HomePage</h1>
-      <p>If you see this, routing works. Next we'll paste your real homepage.</p>
-    </div>
-  );
-}
+export { default } from './ISPHome';
